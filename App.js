3f80da6c5d7a7b36dead04c04fd704b115300da9
Chelsea Farley <chelseafarley91@gmail.com>
2021-12-15 21:07:45 +1300

    Google Auth Demo

--- App.js
+++ App.js
@@ -1,11 +1,56 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, Button } from 'react-native';
+import * as Google from 'expo-google-app-auth';
 
 export default function App() {
+  const [accessToken, setAccessToken] = React.useState();
+  const [userInfo, setUserInfo] = React.useState();
+
+  async function signInWithGoogleAsync() {
+    try {
+      const result = await Google.logInAsync({
+        androidClientId: "663215841316-dkvtp5q5u4oa1m6vkt3m4rkdcoegdg1u.apps.googleusercontent.com",
+        iosClientId: "663215841316-9pd6e3ne5bj7tgisd6o45h0dgoj903av.apps.googleusercontent.com",
+        scopes: ["profile", "email"]
+      });
+
+      if (result.type === "success") {
+        setAccessToken(result.accessToken);
+      } else {
+        console.log("Permission denied");
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  async function getUserData() {
+    let userInfoResponse = await fetch("https://www.googleapis.com/userinfo/v2/me", {
+      headers: { Authorization: `Bearer ${accessToken}`}
+    });
+
+    userInfoResponse.json().then(data => {
+      setUserInfo(data);
+    });
+  }
+
+  function showUserInfo() {
+    if (userInfo) {
+      return (
+        <View style={styles.userInfo}>
+          <Image source={{ uri: userInfo.picture }} style={styles.profilePic} />
+          <Text>Welcome {userInfo.name}</Text>
+          <Text>{userInfo.email}</Text>
+        </View>
+      );
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {showUserInfo()}
+      <Button title={accessToken ? "Get User Data" : "Login"} onPress={accessToken ? getUserData : signInWithGoogleAsync} />
       <StatusBar style="auto" />
     </View>
   );
@@ -18,4 +63,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  userInfo: {
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  profilePic: {
+    width: 50,
+    height: 50
+  }
 });
